0f9036b583529c5b3ce37d5324510d5ecf10993d
Garrett Fuller <garrettec1@gmail.com>
2014-07-08 07:37:24 -0700

    adds style comment
    adds drawGrid

--- tictac_game.py
+++ tictac_game.py
@@ -1,2 +1,14 @@
 #Garrett Fuller
 #Tic-Tac-Toe game
+
+
+#The first thing I want to do, is build a display box
+
+#Style decision
+# X |   |
+# ==|===|==
+#   | X |
+# ==|===|==
+#   |   | X
+
+def drawGrid():
