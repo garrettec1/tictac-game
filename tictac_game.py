847efab000f58cb17f2e6925decf69f386a47cf9
Garrett Fuller <garrettec1@gmail.com>
2014-07-21 17:16:28 -0700

    Adds ftw function
    Extensive formatting for housekeeping. FTW is untested.

--- tictac_game.py
+++ tictac_game.py
@@ -18,19 +18,25 @@ boardInfo = {'1a':' ', '1b':' ', '1c':' ', '2a':' ', '2b':' ', '2c':' ',\
 
 legalMoves = ['1a', '1b', '1c', '2a', '2b', '2c', '3a', '3b', '3c']
 
+win = [('1a','1b','1c'),('2a','2b','2c'), ('3a','3b','3c'), ('1a','2a','3a'),\
+       ('1b','2b','3b'), ('1c','2c','3c'),('1a','2b','3c'), ('3a','2b','1c')]
+
+
 #drawGrid will call the functions that draw individual rows
 def drawGrid():
 
     count = 0
 
     while count < 3:
+
         count+=1
         drawRow(count)
+
         if count <=2:
             drawMid()
 
 
-#Needs to be refactored to build the string with game pieces.
+
 def drawRow(row):
 
     if row == 1:
@@ -39,7 +45,7 @@ def drawRow(row):
     row = str(row)
 
     print("\t{} {} | {} | {}".format(row, boardInfo[row+'a'],\
-                                         boardInfo[row+'b'], boardInfo[row+'c']))
+                                     boardInfo[row+'b'], boardInfo[row+'c']))
 
 
 def drawMid():
@@ -47,45 +53,59 @@ def drawMid():
     print("\t  ==|===|==")
 
 
+
 def getMove():
 
     looping = True
 
     while looping:
-        print("It is your turn to move.")
-        row = str(input("Enter the row number. 1-3: "))
-        column = str(input("Enter the column: a-c: "))
+
+        print("\nIt is your turn to move.")
+
+        row = str(input("\nEnter the row number. 1-3: "))
+        column = str(input("\nEnter the column: a-c: "))
+
         move = (row+column).lower()
 
         if move not in legalMoves:
-            print('That is not a legal move')
+            print('\nThat is not a legal move')
+
         else:
             looping = False
+
     legalMoves.remove(move)
+
 
     return(move)
 
 
+
 def computerMove(boardInfo, legalMoves, turn):
+
     if turn == 1:
         move = '1a'
+
     elif turn == 3:
         move = '3c'
+
     else:
         move = calcMove(boardInfo)
 
     legalMoves.remove(move)
+
+
     return(move)
 
-win = [('1a','1b','1c'),('2a','2b','2c'), ('3a','3b','3c'), ('1a','2a','3a'),\
-       ('1b','2b','3b'), ('1c','2c','3c'),('1a','2b','3c'), ('3a','2b','1c')]
+
 
 def calcMove(boardInfo):
 
 
     move = winBlock('X')
+
     if move not in legalMoves:
         move = winBlock('O')
+
 
     return(move)
 
@@ -94,10 +114,14 @@ def calcMove(boardInfo):
 def winBlock(piece):
 
     move = 0
+
     for three_in_a_row in win:
+
         empty = 0
         counter = 0
+
         for square in three_in_a_row:
+
             if boardInfo[square] == piece:
                 counter+=1
             if boardInfo[square] == ' ':
@@ -107,35 +131,61 @@ def winBlock(piece):
             if move in legalMoves:
                 break
 
+
     return(move)
 
 
-#The first thing that needs to happen after turn 3 is checking for win
-#conditions. First I need to describe win conditions in a list of tipples,
-#then write a function to check for them.
 
-#I think I want to decribe a row as three positions, and check to see if two
-#of three positions are filled.
+def ftw(piece):
+
+    victory = False
+
+    for set in win:
+
+        counter = 0
+
+        for squares in set:
+
+            if boardInfo[squares] == piece:
+                counter += 1
+            if counter == 3:
+                victory = True
+                break
+
+
+    return(victory,piece)
+
+
 
 def updateBoard(move, turn):
 
     print(turn)
+
     if turn % 2 != 0:
         boardInfo[move]= 'X'
+
     else:
         boardInfo[move] = 'O'
 
+
+
 def main():
+
     drawGrid()
+
     gaming = 1
-    while gaming<8:
+
+    while gaming<9:
 
         move = computerMove(boardInfo, legalMoves, gaming)
         updateBoard(move, gaming)
+
         drawGrid()
         gaming+=1
+
         move = getMove()
         updateBoard(move,gaming)
+
         drawGrid()
         gaming += 1
 
@@ -157,3 +207,12 @@ main()
 
 #next I need to find a way to store information about the board so it can
 #be drawn with X's and O's. Ideally this will
+
+#The first thing that needs to happen after turn 3 is checking for win
+#conditions. First I need to describe win conditions in a list of tipples,
+#then write a function to check for them.
+
+#I think I want to decribe a row as three positions, and check to see if two
+#of three positions are filled.
+
+#Finally! I need to check for either a win or a tie.
