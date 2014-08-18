a38c115985035296c75a2d021e054a5a005be364
Garrett Fuller <garrettec1@gmail.com>
2014-08-17 17:58:54 -0700

    Add drawing transparent box around mouse.
    This is terrible.

--- pytictac.py
+++ pytictac.py
@@ -6,9 +6,12 @@ white = (255,255,255)
 black = (0,0,0)
 blue = (0,0,255)
 red = (255,0,0)
+green = (0,225,0)
 
 size = [500,400]
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size,)
+
+
 
 def draw_board():
     pygame.draw.line(screen, blue, [100, 150], [400,150], 5)
@@ -25,17 +28,30 @@ def draw_board():
 
 pygame.display.set_caption("Tictac Game")
 
-screen.fill(white)
+background = screen.fill(white)
+clock = pygame.time.Clock()
+
+background
 
 pygame.display.flip()
 done = False
 while not done:
-
+    clock.tick(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
     draw_board()
+
+    pygame.display.flip()
+
+
+
+    mouse = pygame.mouse.get_pos()
+    box = pygame.Surface((100,100))
+    box.set_alpha(10)
+    box.fill(green)
+    screen.blit(box,mouse)
 
     pygame.display.flip()
 pygame.quit()
