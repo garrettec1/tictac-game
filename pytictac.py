489497a3322101c28a3e3ac79b138fd04fb24074
Garrett Fuller <garrettec1@gmail.com>
2014-08-24 09:40:03 -0700

    Refoactor calc_lines, draw_board, and primary while loop to resize with screen.

--- pytictac.py
+++ pytictac.py
@@ -8,25 +8,34 @@ blue = (0,0,255)
 red = (255,0,0)
 green = (0,225,0)
 
-size = [500,400]
+size = (400,400)
+
 screen = pygame.display.set_mode(size,pygame.RESIZABLE)
 
 def calc_lines(size):
-    x = size
-    return ()
+    """Calculates the whitespace in pixels before the gridlines are drawn if
+    the window is redrawn. Current implementation is just a rough mock up of
+    functionality.
+    """
 
-def draw_board():
-    pygame.draw.line(screen, blue, [100, 150], [400,150], 5)
-    pygame.draw.line(screen, blue, [100, 250], [400,250], 5)
-    pygame.draw.line(screen, blue, [200, 50], [200,350], 5)
-    pygame.draw.line(screen, blue, [300, 50], [300,350], 5)
-    pygame.draw.rect(screen, black, [90,40,320,320], 18)
-    pygame.draw.rect(screen, red, [82,32,10,10])
-    pygame.draw.rect(screen, red, [82,359,10,10])
-    pygame.draw.rect(screen, red, [409,32,10,10])
-    pygame.draw.rect(screen, red, [409,359,10,10])
+    hieght = size[1]
+    width = size[0]
+    box_size_down = int(hieght/3)
+    box_size_right = int(width/3)
 
+    down1 = ((1, box_size_down),(width, box_size_down))
+    down2 = ((1, box_size_down*2), (width, box_size_down*2))
+    right1 = ((box_size_right, 1), (box_size_right, hieght))
+    right2 = ((box_size_right*2, 1), (box_size_right*2, hieght))
 
+    lines = [down1,down2,right1,right2]
+    return (lines)
+
+def draw_board(size):
+
+    lines = calc_lines(size)
+    for position in lines:
+        pygame.draw.line(screen, blue, position[0], position[1], 20)
 
 pygame.display.set_caption("Tictac Game")
 
@@ -34,34 +43,33 @@ background = screen.fill(white)
 clock = pygame.time.Clock()
 
 background
+draw_board(size)
 
 pygame.display.flip()
 done = False
 while not done:
-    clock.tick(10)
+    clock.tick(20)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.VIDEORESIZE:
-            size = (event.h, event.w)
-
-
-            #Event(16-VideoResize {'h': 501, 'w': 612, 'size': (612, 501)})>
-    draw_board()
+            size = (event.w, event.h)
+            screen = pygame.display.set_mode(size,pygame.RESIZABLE)
+            screen.fill(white)
+            draw_board(size)
 
     pygame.display.flip()
 
-    test = []
-
-
-    mouse = pygame.mouse.get_pos()
+    '''mouse = pygame.mouse.get_pos()
     box = pygame.Surface((100,100))
     box.set_alpha(10)
     box.fill(green)
-    screen.blit(box,mouse)
+    screen.blit(box,mouse)'''
 
     pygame.display.flip()
+
+
 pygame.quit()
 
 #sys.exit()
